528fe3fe44fa0705031811c751aed075e848866c
Matiasgma <matiasgma95@gmail.com>
2023-11-02 11:25:32 -0300

    first commit

---
+++ models/User.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const secret = process.env.SECRET_JWT
+const tokenExpiration =process.env.TOKEN_EXPIRATION
+const saltOrRounds = process.env.SALT_OR_ROUNDS
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: { type: String },
+    dob: { type: Date },
+    mail: { type: String },
+    password: { type: String }
+})
+
+userSchema.methods.hashPassword = function (password) {
+    this.password = bcrypt.hashSync(password, saltOrRounds)
+}
+
+userSchema.methods.generateJWT = function () {
+    return jwt.sign({ userId: this._id }, secret, { expiresIn: tokenExpiration })
+}
+
+userSchema.methods.onSignUpGenerateJWT = function () {
+    return {
+        userId: this._id,
+        token: this.generateJWT()
+    }
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
